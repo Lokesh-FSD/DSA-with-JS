a2a06007a76ad50c80a546b28499dfffb03b6511
lokeshtrentium <lokesh@trentiums.com>
2023-07-30 14:28:08 +0530

    first commit

--- loops-Patterns.js
+++ loops-Patterns.js
@@ -1,1 +1,62 @@
-console.log("hello DSA");
+// print a pattern of start for (4 x 4) star pattern
+
+// ****
+// ****
+// ****
+// ****
+
+// let n = 4;
+
+// for (let i = 1; i <= n; i++) {
+//   let str = "";
+//   for (let j = 1; j <= n; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+// ------------------------------------------------------------------------------------------
+
+//  # Print the folowing pattern of star with the value of n given;
+
+// *
+// **
+// ***
+// ****
+
+// code
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+// ------------------------------------------------------------------------------------------------
+
+// # Print the below star pattern
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+//   let str = "";
+//   let space = n - i;
+//   // concatinate " " to str
+//   for (let j = 1; j <= space; j++) {
+//     str += " ";
+//   }
+//   // concatinate * to the str
+//   for (let k = 1; k <= i; k++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+// ---------------------------------------------
